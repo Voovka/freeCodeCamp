6a348b5b8d12180770262dd947858a0a09a190fe
Voovka <eclipse23_94@mail.ru>
2019-05-01 11:39:54 +0300

    sollution Commit

--- phoneValidator.js
+++ phoneValidator.js
@@ -1,15 +1,35 @@
 function telephoneCheck(str) {
     // Good luck!
-    return true;
+
+    let regExp = /\D/g;
+
+
+    // проверка скобки
+    if (str.replace(/[^()]/g, "").length > 0) {
+        if (str.replace(/[^()]/g, "").length !== 2 || (str.replace(/[^()]/g, "x").indexOf(")") - str.replace(/[^()]/g, "x").indexOf("(") !== 4)) {
+            console.log("return false by ()");
+            return false;
+        }
+    }
+
+
+    if (/^\d|^\(/.test(str)) {
+        if (str.replace(regExp, "").length === 11 && str.replace(regExp, "").substr(0, 1) === "1") {
+            return true;
+        } else if (str.replace(regExp, "").length === 10) {
+            return true;
+        } else {
+            return false
+        }
+    } else {
+        console.log("return false by first symbol");
+        return false;
+    }
+
 }
 
-telephoneCheck("555-555-5555");
 
-
-
-// 555-555-5555
-// (555)555-5555
-// (555) 555-5555
-// 555 555 5555
-// 5555555555
-// 1 555 555 5555
+function telephoneCheckSolution(str) {
+    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+}
